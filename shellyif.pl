df6a47a1eaf1a9c283d6049428f9ede5f4b2a223
Erik Slagter <erik@openpli.org>
2024-03-09 13:38:07 +0100

    Allow for more then one plug to be questioned or monitored.

--- shellyif.pl
+++ shellyif.pl
@@ -6,7 +6,7 @@ use Data::Dumper;
 use DateTime;
 use POSIX qw(sleep);
 
-use constant { usage_string => "usage: shellyif.pl [-w|--watch] [-h|-?|--help] <host>\n    watch = repeat endlessly\n" };
+use constant { usage_string => "usage: shellyif.pl [-w|--watch] [-h|-?|--help] <host> [<host>]\n    watch = repeat endlessly\n" };
 
 my($option_help) = 0;
 my($option_watch) = 0;
@@ -17,27 +17,95 @@ $result = $getopt->getoptions("w|watch" => \$option_watch, "h|?|help"  => \$opti
 die(usage_string) if(!$result || $option_help);
 die(usage_string) if(scalar(@ARGV) eq 0);
 
-my($error);
-my($shellyif);
+my($error, $host, $host2);
+my($shellyif, %shellyif);
+my($total_power, $total_current);
 
 STDOUT->autoflush(1);
 
-$shellyif = new Shelly::IF($ARGV[0]);
-$error = $shellyif->get_error();
-die($error) if(defined($error));
+while(defined($host = shift(@ARGV)))
+{
+	$shellyif = new Shelly::IF($host);
+	$error = $shellyif->get_error();
+	die($error) if(defined($error));
 
-printf("%s\n", $shellyif->dump_header());
+	$host2 = $host;
+	$shellyif{$host} = $shellyif;
+}
+
+if($option_watch && (scalar(keys(%shellyif)) > 1))
+{
+	for(;;)
+	{
+		printf("%s\n", $shellyif->dump_header());
+		$total_power = 0;
+		$total_current = 0;
+
+		for $host (sort(keys(%shellyif)))
+		{
+			$shellyif = $shellyif{$host};
+			printf("%s\n", $shellyif->dump());
+			$total_power += $shellyif->get_power();
+			$total_current += $shellyif->get_current();
+		}
+
+		if(scalar(keys(%shellyif)) > 1)
+		{
+			printf("%-16s %-13s %-16s %8.1f %8s %7.1f\n",
+				"TOTAL",
+				"",
+				"",
+				$total_power,
+				"",
+				$total_current);
+		}
+
+		sleep(1);
+
+		for $host (sort(keys(%shellyif)))
+		{
+			$shellyif = $shellyif{$host};
+			die($shellyif->get_error()) if(!$shellyif->update());
+		}
+	}
+
+	exit(0);
+}
 
 if($option_watch)
 {
+	$shellyif = $shellyif{$host2};
+
+	printf("%s\n", $shellyif->dump_header());
+
 	for(;;)
 	{
 		printf("%s\r", $shellyif->dump());
 		sleep(1);
 		die($shellyif->get_error()) if(!$shellyif->update());
 	}
+
+	exit(0);
 }
-else
+
+$shellyif = $shellyif{$host2};
+printf("%s\n", $shellyif->dump_header());
+
+for $host (sort(keys(%shellyif)))
 {
+	$shellyif = $shellyif{$host};
+	$total_power += $shellyif->get_power();
+	$total_current += $shellyif->get_current();
 	printf("%s\n", $shellyif->dump());
+}
+
+if(scalar(keys(%shellyif)) > 1)
+{
+	printf("%-16s %-13s %-16s %8.1f %8s %7.1f\n",
+		"TOTAL",
+		"",
+		"",
+		$total_power,
+		"",
+		$total_current);
 }
